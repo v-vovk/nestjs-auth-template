b41a629245706a4270c256abdbbe2453e9b3c2d3
Vlad Vovk <wolfykey@gmail.com>
2022-01-30 17:29:09 +0200

    added jwt, refresh and access tokens

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,21 +9,21 @@ export class AuthController {
 
   @Post('/local/signup')
   signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
-    this.authService.signupLocal(dto);
+    return this.authService.signupLocal(dto);
   }
 
   @Post('/local/signin')
   signinLocal() {
-    this.authService.signinLocal();
+    return this.authService.signinLocal();
   }
 
   @Post('/logout')
   logout() {
-    this.authService.logout();
+    return this.authService.logout();
   }
 
   @Post('/refresh')
   refreshTokens() {
-    this.authService.refreshTokens();
+    return this.authService.refreshTokens();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,14 +3,11 @@ import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
-
-  hashData(data: string) {
-    return bcrypt.hash(data, 10);
-  }
+  constructor(private prisma: PrismaService, private jwtService: JwtService) {}
 
   async signupLocal(dto: AuthDto): Promise<Tokens> {
     const hash = await this.hashData(dto.password);
@@ -21,6 +18,12 @@ export class AuthService {
         hash,
       },
     });
+
+    const tokens = await this.getTokens(newUser.id, newUser.email);
+
+    await this.updateRefTokHash(newUser.id, tokens.refresh_token);
+
+    return tokens;
   }
 
   signinLocal() {}
@@ -28,4 +31,51 @@ export class AuthService {
   logout() {}
 
   refreshTokens() {}
+
+  async updateRefTokHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hashedRefTok: hash,
+      },
+    });
+  }
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+
+  async getTokens(userId: number, email: string): Promise<Tokens> {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15, // 15 min
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 24 * 7, // 1 week
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
 }
